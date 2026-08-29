090b397591c179f5081ca3e4a30d611db78df242
tyza66 <shun_@outlook.com>
2026-08-29 12:43:39 +0800

    feat: 组件版本号提升到 1.0.20260829

--- platformio_ide/src/data_base.c
+++ platformio_ide/src/data_base.c
@@ -16,7 +16,7 @@ fe_output_t data_base_dispatch(void *inst, const char *act, const char *args) {
     }
     if (strcmp(act, "info") == 0) {
         return fe_ok(act,
-            "{\"name\":\"BaseData\",\"firmware\":\"FasterEdge-MCU 0.1.0\","
+            "{\"name\":\"BaseData\",\"firmware\":\"FasterEdge-MCU 1.0.20260829\","
             "\"chip\":\"ESP8266\",\"sdk\":\"Keil/C\"}");
     }
     return fe_err(act, "unsupported command");
